1de89229efdf2abc915f039c36e92d4af127034e
Paris Bland <paris@pariselectra.com>
2022-11-02 00:27:24 -0500

    final touches

--- Develop/generateList.js
+++ Develop/generateList.js
@@ -48,7 +48,7 @@ module.exports = templateData => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="./style.css">
+        <link rel="stylesheet" href="/Users/parisbland/Bootcamp/Challenges/Employee-List-Generator/Develop/assets/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
         <title>Team Profiles</title>
          <link rel="stylesheet" href="style.css">
